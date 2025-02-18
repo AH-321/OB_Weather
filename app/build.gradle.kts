00a96473343f33f18353648ebae484acd33b201e
Aiden Haglund <116464553+AH-321@users.noreply.github.com>
2025-02-18 15:25:14 -0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.1a"
+        versionName = "0.2.0a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
